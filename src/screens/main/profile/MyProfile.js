3dfb2a6950d54809b286a1ea91893fc990c994e5
Sachin <sac123sh@gmail.com>
2023-09-05 15:24:16 +0530

    ddd

--- src/screens/main/profile/MyProfile.js
+++ src/screens/main/profile/MyProfile.js
@@ -1,4 +1,13 @@
-import {View, Text, StyleSheet, Image, TextInput, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
 import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BackHandler from '../../../component/BackHandler';
@@ -9,9 +18,14 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import CustomDropDown from '../../../component/common/CustomDropDown';
 import Button from '../../../component/customButton/Button';
 import {icon} from '../../../utils/Image';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import Text16 from '../../../component/customText/Text16';
+import LottieView from 'lottie-react-native';
 
 const MyProfile = () => {
   const [edit, setEdit] = useState(false);
+  const [uploadModal,setUploadModal]=useState(false)
+  const [url,setUrl]=useState('')
   const [profileData, setProfileData] = useState({
     firstName: 'Sachin',
     lastName: 'Kumar',
@@ -28,6 +42,9 @@ const MyProfile = () => {
     }));
   };
 
+
+
+
   return (
     <View style={CommonStyle.container}>
       <BackHandler name={'My Profile'} />
@@ -35,7 +52,9 @@ const MyProfile = () => {
         <View style={styles.conatiner}>
           {
             //#region image container
-            <View style={styles.imgConatiner}>
+            <TouchableOpacity
+              onPress={() => setUploadModal(true)}
+              style={styles.imgConatiner}>
               <Image
                 source={{
                   uri: 'https://nftcrypto.io/wp-content/uploads/2023/05/2023-05-18-17_57_18-The-Journey-of-an-NFT-Avatar-Word-Product-Activation-Failed.png',
@@ -53,7 +72,7 @@ const MyProfile = () => {
                 }}
                 source={icon.edit}
               />
-            </View>
+            </TouchableOpacity>
             //#endregion
           }
 
@@ -130,6 +149,12 @@ const MyProfile = () => {
           text={edit ? 'Save' : 'Edit Profile'}
         />
       </View>
+      <Upload 
+      uploadModal={uploadModal} 
+      setUploadModal={setUploadModal}
+      setUrl={setUrl}
+       />
+
     </View>
   );
 };
@@ -156,6 +181,7 @@ const InputWithHeading = ({heading, edit, value, onChangeText}) => {
           style={{fontFamily: fonts.regular, color: colors.gray}}
         />
       </View>
+
     </View>
   );
 };
@@ -177,3 +203,87 @@ const styles = StyleSheet.create({
   },
 });
 export default MyProfile;
+
+const Upload = ({uploadModal,setUploadModal,setUrl}) => {
+  const uploadFromCamera = async () => {
+    // alert('hello')
+    let options = {
+      mediaType: 'photo',
+      cameraType: 'back',
+      saveToPhotos: true,
+    };
+
+    launchCamera(options, response => {
+      if (response.didCancel) {
+        // toast('User cancelled ', colorConstant.darkRed)
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        // toast('Something went wrong', colorConstant.darkRed)
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        console.log('response', response);
+        setUploadModal(false)
+      }
+    });
+  };
+
+
+  const uploadFromGallery = async () => {
+    let options = {
+      mediaType: 'photo',
+      cameraType: 'back',
+      saveToPhotos: true,
+    };
+    launchImageLibrary(options, response => {
+      console.log('response', response);
+      setUploadModal(false)
+    });
+  };
+
+  return (
+    <Modal animationType='slide' visible={uploadModal} transparent statusBarTranslucent>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'rgba(0,0,0,0.2)',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            width: '90%',
+            backgroundColor: colors.white,
+            // height: moderateScale(180),
+            borderRadius: 10,
+            justifyContent:'center',
+            alignItems:'center',
+            paddingBottom:moderateScale(20),
+            paddingTop:moderateScale(10)
+          }}>
+
+            
+            <LottieView height={moderateScale(100)} width={moderateScale(100)} source={require('../../../assets/upload.json')} autoPlay loop />
+            <View style={{flexDirection:'row',justifyContent:'space-around',width:'100%'}}>
+
+
+            
+          <TouchableOpacity 
+          onPress={()=>uploadFromGallery()}
+          style={{width:'42%',backgroundColor:colors.yellow,paddingVertical:moderateScale(10),borderRadius:8,alignItems:'center'}}>
+            <Text14 fontFamily={fonts.regular} mt={1} color={colors.theme} text={'Upload from gallery'} />
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+          onPress={()=>uploadFromCamera()}
+          style={{borderWidth:1,borderColor:colors.yellow,width:'42%',borderRadius:8,justifyContent:'center',alignItems:'center'}}>
+            <Text14 fontFamily={fonts.regular} mt={1} text={'Take a picture'} />
+          </TouchableOpacity>
+            
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
